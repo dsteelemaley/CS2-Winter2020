86fcd14e2525b639cf534aa393f6cdaa5fa028a7
dsteelemaley <44673794+dsteelemaley@users.noreply.github.com>
2020-01-08 09:27:35 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,4 @@
 #Python3
-#------
 #Define a function "fibonacci"
 def fibonacci(n):
 	#initialize the list to store the sequence
@@ -9,7 +8,18 @@ def fibonacci(n):
 		#append the new number to the end of the list
 		x.append(x[-1]+x[-2])
 	#return the list containing the sequence
-	return x
+	#I decided to not write an if statement, and I know this probably slower than a fixed case
+	#computationally but this solution is fun.
+	#List comprehension!
+	return [i for i in x if i < n]
+
+	'''
+	#The more simple solution
+	if n == 1:
+		return [0]
+	else:
+		return x
+	'''
 
 #print the fibonacci sequence under 100
 print(fibonacci(100))
